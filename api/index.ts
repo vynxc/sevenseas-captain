7a15179905ba181368de00496f4e7f3ba1157582
Tommy <tommydanks2000@outlook.com>
2024-03-14 21:44:40 +0000

    maybe

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,3 @@
-import { app } from '../src/utils/app';
-
 export const config = { runtime: 'edge' };
 
-export default async function handler(request: Request) {
-  return app.fetch(request);
-}
+import '../src/utils/app';
