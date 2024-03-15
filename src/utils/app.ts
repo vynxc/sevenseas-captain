2d77aa385d58012a3e2aa0ed7a1fa129d2eb1950
chayc <svynxc@gmail.com>
2024-03-14 23:46:49 -0700

    remove cors

--- src/utils/app.ts
+++ src/utils/app.ts
@@ -1,7 +1,6 @@
-import cors from '@elysiajs/cors';
 import {Elysia} from 'elysia';
 
-const app = new Elysia().use(cors());
+const app = new Elysia();
 export type AppType = typeof app;
 
 export {app};
